5ba3b6eb3841e1c1ada228432cccd99db5b95081
Kristaps <klaukmanis@gmail.com>
2022-02-25 20:57:02 +0200

    Jaunais materiāls

---
+++ src/main/java/io/codelex/arrays/practice/Exercise2.java
@@ -0,0 +1,19 @@
+package io.codelex.arrays.practice;
+
+public class Exercise2 {
+
+    //TODO: Write a Java program to sum values of an array.
+    public static void main(String[] args) {
+        int myArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int sum = 0;
+
+        /*
+        fixme
+        for (........) {
+        ........
+        }
+        */
+
+        System.out.println("The sum is " + sum);
+    }
+}
